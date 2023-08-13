e8c41f73a658a173d7195d851050df81b2c24710
code-with-pb <codexpythonpb@gmail.com>
2023-08-13 16:12:07 +0530

    A7

---
+++ Basic_I/A7.java
@@ -0,0 +1,18 @@
+
+public class A7 {
+	void Cal() {
+		int a = 8;
+		int i = 1;
+		do {
+			System.out.println("Table of 8: "+(a*i));
+			
+			i++;
+		}while(i < 11);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A7 o = new A7();
+		o.Cal();
+	}
+
+}

6a6aa882ac1f58c8c4a763bb8212541c8deaeeb6
code-with-pb <codexpythonpb@gmail.com>
2023-08-13 23:08:12 +0530

    Left Traingle

---
+++ Basic_I_Pattern_I/P3.java
@@ -0,0 +1,18 @@
+
+public class P3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int s = 5;
+		for(int i=0;i<s;i++) {
+			
+			for(int j=0;j<i;j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}

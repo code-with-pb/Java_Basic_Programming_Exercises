2399efb84fe037bcce3edc584e49a878483785ab
code-with-pb <codexpythonpb@gmail.com>
2023-08-13 16:30:47 +0530

    Square Pattern 1

--- Basic_I_Pattern_I/P1.java
+++ Basic_I_Pattern_I/P1.java
@@ -3,8 +3,9 @@ public class P1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i<5; i++) {
-			for(int j = 0;j<5;j++) {
+		int s = 5;
+		for(int i = 0; i<s; i++) {
+			for(int j = 0;j<s;j++) {
 				System.out.print("*");
 			}
 			System.out.println();

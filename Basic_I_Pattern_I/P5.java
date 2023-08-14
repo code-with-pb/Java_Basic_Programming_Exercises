50ab4c0a6367876e2ec702ddca271202e8953850
code-with-pb <codexpythonpb@gmail.com>
2023-08-15 00:14:40 +0530

    Left Down Triangle

---
+++ Basic_I_Pattern_I/P5.java
@@ -0,0 +1,17 @@
+
+public class P5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int s = 5;
+		
+		for(int a=0;a<s;a++) {
+			for(int b=0;b<s-a;b++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
